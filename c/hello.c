f66854d8c4693c82c3c8718e2d4f5ef85f205b83
lidaning <453882101@qq.com>
2022-04-28 19:36:20 +0800

    add enum demo

--- c/hello.c
+++ c/hello.c
@@ -4,7 +4,27 @@
 #include "stdio.h"
 #include "hello.h"
 
+enum Sex{
+    MALE,
+    FEMALE,
+    SECRET
+};
+
 void hello(const char* name){
-    printf("hello my name is %s\n", name);
+
+    enum Sex s = FEMALE;
+
+    printf("hello my name is %s, I'm a %d\n", name, s);
+
+    printf("length of char is:%d\n", sizeof(char));
+    printf("length of int is:%d\n", sizeof(int));
+    printf("length of float is:%d\n", sizeof(float));
+    printf("length of long is:%d\n", sizeof(long));
+
+    int age=20;
+    printf("my age is:%d.", age);
+
 }
+
+
 
